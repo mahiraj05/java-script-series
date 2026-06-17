799ded72da7ffab52011b3d87aab14df139d7dce
Maahi Raj <maahiiraj210@gmail.com>
2026-06-17 15:02:00 +0000

    datatypes.js

---
+++ .devcontainer/01_variable.js
@@ -0,0 +1,13 @@
+const accountID =23210508
+let accountEmail ="mahi@2321@gamil.com"
+var accountPassword ="12345"
+accountCity ="jaipur"
+
+
+accountEmail = "mahii@c.com"
+accountPassword = "2y49887439"
+accountCity ="patna"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
