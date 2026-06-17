6fe3e42643b261e1b10af9dd09e82fbf91eab8ef
Maahi Raj <maahiiraj210@gmail.com>
2026-06-17 15:27:40 +0000

    conversionOperation

---
+++ .devcontainer/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 0
+
+let booleanIsloggedIn =  Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
